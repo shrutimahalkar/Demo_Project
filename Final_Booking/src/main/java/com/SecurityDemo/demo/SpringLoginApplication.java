6d9aa898c643489044d563a9e025512a65e4ce4e
shubhu <shubhamgherde10@gmail.com>
2019-11-12 19:21:18 +0530

    changes in main

--- Final_Booking/src/main/java/com/SecurityDemo/demo/SpringLoginApplication.java
+++ Final_Booking/src/main/java/com/SecurityDemo/demo/SpringLoginApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringLoginApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(SpringLoginApplication.class, args);
 	}
 
